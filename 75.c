c61c715edb943accb2cc706a1bd909ea8a369186
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-10 22:39:56 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,18 @@
+int main() 
+{
+	char str[30];
+	int n,i;
+	scanf("%s",str);
+	n=strlen(str);
+	if(n%2==0)
+	{
+		str[n/2]='*';
+		str[(n/2)-1]='*';
+	}
+	else
+	{
+		str[n/2]='*';
+	}
+	printf("%s",str);
+	return 0;
+}
