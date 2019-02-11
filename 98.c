f7ede3dfa070182cb9dff7ee8f84cbb90eef0ff7
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-11 20:42:45 +0530

    Create 98.c

---
+++ 98.c
@@ -0,0 +1,18 @@
+int main() {
+   int n;
+   int i,a[100],j;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+   for(i=0;i<n;i++)
+   {
+       for(j=i+1;j<n;j++)
+       {
+       if(a[i]>a[j])
+       {
+           printf("%d",i);
+       }
+   }
+   }
+   return 0;
+}
