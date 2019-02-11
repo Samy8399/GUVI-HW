f654ede071b3481487ac96cff99eb134d4e5c995
Samy8399 <46838475+Samy8399@users.noreply.github.com>
2019-02-11 10:03:48 +0530

    Rename 79.c to 88.c

---
+++ 88.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+scanf("%d%d",&a,&b);
+if(a<b)
+{a=a+b;
+b=a-b;
+a=a-b;
+} d=a;
+printf("%d\t%d",a,b);
+while(a)
+{
+if((a%d==0)&&(a%b==0))
+{
+ c=a;
+ a=-1;
+}
+a++;
+}
+printf("\n%d",c);
+getch();
+}
